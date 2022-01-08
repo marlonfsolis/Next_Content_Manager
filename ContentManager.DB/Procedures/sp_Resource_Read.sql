91311de3a4416c2d67429fe2ceee205a64bef8a4
marlonfsolis <marlonfsolis@outlook.com>
2022-01-08 06:10:05 -0500

    Resource read done.

--- ContentManager.DB/Procedures/sp_Resource_Read.sql
+++ ContentManager.DB/Procedures/sp_Resource_Read.sql
@@ -1,6 +1,112 @@
-﻿CREATE PROCEDURE [dbo].[sp_Resource_Read]
-	@param1 int = 0,
-	@param2 int
+﻿-- =============================================
+-- Author: Marlon Fernandez
+-- Create date: 2022-01-08
+-- Description:	Read one Resource by ResourceId
+-- Updates:
+-- Developer Name - Date - Description of the update
+-- =============================================
+CREATE PROCEDURE [dbo].[sp_Resource_Read]
+	@resourceId INT = 0,
+	@errorCode INT = 0 OUTPUT,
+	@errorLogId INT = 0 OUTPUT
 AS
-	SELECT @param1, @param2
-RETURN 0
+BEGIN TRY
+	SET NOCOUNT ON
+	SET @errorCode = 0
+	SET @errorLogId = 0
+
+	-- Local variables
+	DECLARE @ProcedureName VARCHAR(100) = 'sp_Resource_Read'
+	DECLARE @LogMessage TABLE(LogMessage VARCHAR(MAX), LogDate DATETIME)
+	DECLARE @ErrorMsg VARCHAR(500)
+	DECLARE @LocalTranStarted bit = 0
+
+    INSERT INTO @LogMessage VALUES (@ProcedureName+' START', GETDATE())
+
+	INSERT INTO @LogMessage VALUES ('ParameterList:' +
+									'resourceId: ' + ISNULL(CAST(@resourceId AS VARCHAR), 'NULL') + ' || ' +
+									'ProfileId: ' + ISNULL(CAST(0 AS VARCHAR), 'NULL')
+									, GETDATE())
+
+
+	----------------------------
+	/* PRE-VALIDATION SECTION */
+	----------------------------
+
+	INSERT INTO @LogMessage VALUES ('[PRE-VAL] START', GETDATE());
+	
+	-- Place the data validation here --
+	IF ISNULL(@resourceId,0) = 0
+	BEGIN
+		INSERT INTO @LogMessage VALUES ('[ERROR] resourceId IS NULL', GETDATE())
+		;Throw 51000, '[ERROR] resourceId IS NULL', 1
+	END
+
+	--------------------------------
+	/* END PRE-VALIDATION SECTION */
+	--------------------------------
+
+
+	-- Begin Transaction to allow rollback
+	IF @@TRANCOUNT = 0
+	BEGIN
+		BEGIN TRANSACTION @Procedurename 
+		SET @LocalTranStarted = 1
+	END
+
+
+	SELECT r.ResourceId
+		  ,r.Title
+		  ,r.Description
+		  ,r.Link
+		  ,r.ImageUrl
+		  ,r.Priority
+		  ,r.TimeToFinish
+		  ,r.Active
+		  ,r.CreatedAt
+	FROM Resource r
+	WHERE r.ResourceId = @resourceId
+	
+
+
+	-- Commint transaction
+	IF @LocalTranStarted = 1 and @@TRANCOUNT > 0	
+	BEGIN
+		COMMIT TRANSACTION @ProcedureName		
+	END
+
+
+	INSERT INTO @LogMessage VALUES (@ProcedureName+' END', GETDATE())
+
+END TRY
+
+BEGIN CATCH
+	-- Rollback any changes if error occurs only when local transaction has occurred
+	IF @LocalTranStarted = 1 and @@TRANCOUNT > 0
+	BEGIN
+		ROLLBACK TRANSACTION @ProcedureName
+	END
+
+	SET @ErrorMsg = 'STORED PROC ERROR'
+	+ ' - PROC: ' + ISNULL(@ProcedureName, 'N/A')
+	+ ' - LINE: ' + CAST(ISNULL(ERROR_LINE(), 0) AS VARCHAR(50))
+	+ ' - MSG: ' + ERROR_MESSAGE()
+
+
+	-- Write Error logs kept through SP
+	INSERT INTO ErrorLog (ErrorMessage, ErrorDetails, StackTrace, ErrorDate)
+		VALUES (@errorMsg, '', '', GETDATE())
+
+	SELECT @errorLogId = SCOPE_IDENTITY()
+	INSERT INTO ErrorLogTrace (ErrorLogId, TraceMessage, TraceDate)
+		SELECT
+			@errorLogId
+		   ,LogMessage
+		   ,LogDate
+		FROM @LogMessage
+
+	-- Set @errorCode to 1 to return failure to UI
+	IF @errorCode = 0 
+		SET @errorCode = 1;
+
+END CATCH
