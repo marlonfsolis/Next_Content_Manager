5585b9e0ee087e79ea47af086cb90ba6b2e5b697
Marlon Fernandez <marlonfsolis@outlook.com>
2022-02-21 21:00:16 -0500

    Fixing and formating the About page.

--- ContentManager.Client/pages/home/about.js
+++ ContentManager.Client/pages/home/about.js
@@ -1,25 +1,32 @@
 import Layout from "components/Layout";
-import useSWR from 'swr';
+import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function About() {
-
   /**
-   * Fetching data on client side. This will fetch tha data 
+   * Fetching data on client side. This will fetch tha data
    * on client side after the page is rendered.
    * Good for Dashboards and Internal pages that do not need the SEO feature.
    * Install swr: npm install swr
    * THen use it like here
    */
-  const {data, error} = useSWR('../api/about', fetcher);
-  
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
+  const { data, error } = useSWR("../api/about", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
 
   return (
     <Layout>
-    <h1 className="h1">{data.message}</h1>
+      <div className="container">
+        <section className="hero">
+          <div className="hero-body">
+            <p className="title">About Content Manager</p>
+            <p className="subtitle">An app to learn Next.js</p>
+            <p className="content">{data.message}</p>
+          </div>
+        </section>
+      </div>
     </Layout>
   );
 }
