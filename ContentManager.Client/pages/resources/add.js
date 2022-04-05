45cb6950718d72d51ba37440f428f2d4f48cdc41
marlonfsolis <marlonfsolis@outlook.com>
2022-04-05 07:21:29 -0400

    Add resource form design done.

--- ContentManager.Client/pages/resources/add.js
+++ ContentManager.Client/pages/resources/add.js
@@ -1,99 +1,84 @@
 import Layout from "components/Layout";
-import { useEffect } from "react";
-import { TextBoxComponent } from "@syncfusion/ej2-react-inputs";
-
-import { Form, Field, FormElement } from "@progress/kendo-react-form";
-import { Error } from "@progress/kendo-react-labels";
-import { Input } from "@progress/kendo-react-inputs";
-
-const emailValidator = (value) => {
-	const emailRegex = new RegExp(/\S+@\S+\.\S+/);
-	return emailRegex.test(value) ? "" : "Please enter a valid email.";
-}
-
-const requiredValidator = (value) => {
-	return value ? "" : "Field required.";
-}
-
-const EmailInput = (fieldRenderProps) => {
-	const { validationMessage, visited, ...others } = fieldRenderProps;
-	return (
-		<div>
-			<Input {...others} />
-			{
-				visited && validationMessage
-					? <Error>{validationMessage}</Error>
-					: <label>Personal email address.</label>
-			}
-		</div>
-	);
-};
+import { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function AddResourcePage() {
 
 	const handleSubmit = (dataItem) => alert(JSON.stringify(dataItem, null, 2));
 
+	const [timeToFinish, setTimeToFinish] = useState(120);
+
 	useEffect(() => {
 		// addResource();
 	});
 
+	const timeToFinish_ChangeHandler = (e) => {
+		const time = e.target.value;
+		setTimeToFinish(time);
+	};
+
 	return (
 		<Layout>
-			<div className="columns">
-				<div className="column is-offset-3 is-6">
-					<h1 className="title">Create New Resource</h1>
+			<Form className="col-8 mx-auto">
+				<Row className="mb-5">
+					<Col className="d-flex justify-content-center">
+						<h1>Create New Resource</h1>
+					</Col>
+				</Row>
 
-					<div className="field">
-						<label className="label">Title</label>
-						<div className="control">
-							<input className="input" type="text" placeholder="Text input" />
-						</div>
-					</div>
+				<Row className="mb-3">
+					<Form.Group as={Col}>
+						<Form.Label>Title</Form.Label>
+						<Form.Control type="text" placeholder="Enter resource title" />
+					</Form.Group>
 
-					<h3 className="title">KendoUI Form</h3>
-					<Form
-						onSubmit={handleSubmit}
-						render={(formRenderProps) => (
-							<FormElement
-								style={{
-									maxWidth: 650,
-								}}
-							>
-								<fieldset className={"k-form-fieldset"}>
-									<legend className={"k-form-legend"}>
-										Please fill in the fields:
-									</legend>
+					<Form.Group as={Col}>
+						<Form.Label>Description</Form.Label>
+						<Form.Control type="text" placeholder="Describe the resource" />
+					</Form.Group>
+				</Row>
 
-									<div className="mb-3">
-										<Field name={"firstName"} component={Input} label={"First name"} />
-									</div>
+				<Form.Group as={Col} className=" mb-3">
+					<Form.Label>Link</Form.Label>
+					<Form.Control type="text" placeholder="Link" />
+				</Form.Group>
 
-									<div className="mb-3">
-										<Field name={"lastName"} component={Input} label={"Last name"}
-											validator={requiredValidator} />
-									</div>
+				<Form.Group as={Col} className=" mb-3">
+					<Form.Label>Image</Form.Label>
+					<Form.Control type="text" placeholder="Enter the url of the image" />
+				</Form.Group>
 
-									<div className="mb-3">
-										<Field name={"email"} component={EmailInput} label={"Email"}
-											type={"email"}
-											validator={emailValidator} />
-									</div>
-								</fieldset>
+				<Row>
+					<Form.Group as={Col} className=" mb-3">
+						<Form.Label>Priority</Form.Label>
+						<Form.Select type="text" aria-label="What is the priority for the resource">
+							<option value={-1}>Select one</option>
+							<option value={1}>1</option>
+							<option value={2}>2</option>
+							<option value={3}>3</option>
+						</Form.Select>
+					</Form.Group>
 
-								<div className="k-form-buttons">
-									<button
-										type={"submit"}
-										className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base"
-										disabled={!formRenderProps.allowSubmit}
-									>
-										Submit
-									</button>
-								</div>
-							</FormElement>
-						)}
-					/>
-				</div>
-			</div>
+					<Form.Group as={Col}>
+						<Form.Label>Created At</Form.Label>
+						<Form.Control type="date" />
+					</Form.Group>
+				</Row>
+
+				<Form.Group as={Col} className=" mb-3">
+					<Form.Label>Time To Finish</Form.Label>
+					<Form.Range min="0" max="1440" onChange={timeToFinish_ChangeHandler} value={timeToFinish} />
+					<Form.Text>{timeToFinish} minutes</Form.Text>
+				</Form.Group>
+
+				<Form.Group as={Col}>
+					<Form.Check type="switch" label="Active" id="activeStatus" />
+				</Form.Group>
+
+			</Form>
+
 		</Layout>
 	);
 }
