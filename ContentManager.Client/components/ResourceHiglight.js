bc2033aaffc130f254de213143bddbe6f5411171
Marlon Fernandez <marlonfsolis@outlook.com>
2022-04-23 15:41:17 -0400

    Buttons added.

--- ContentManager.Client/components/ResourceHiglight.js
+++ ContentManager.Client/components/ResourceHiglight.js
@@ -23,6 +23,13 @@ export default function ResourceHiglight({ resources }) {
                   <div className="card-body text-dark">
                     <h5 className="card-title">{r.title}</h5>
                     <p className="card-text">{r.description}</p>
+                    <div className="d-grid gap-2 d-sm-block">
+                      <button className="btn btn-info col-sm-4"
+                        type="button"
+                        onClick={() => navigateTo("/resources/details")}>Details</button>
+                      <button className="btn btn-warning col-sm-4 ms-sm-2"
+                        type="button">Edit</button>
+                    </div>
                   </div>
                 </div>
               </div>
